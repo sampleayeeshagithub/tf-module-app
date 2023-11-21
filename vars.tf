9c3b01a73c7686ec6cbe2f7ae47416dc684731b2
Mulla Ayeesha <ayeeshamulla0708@gmailcom>
2023-11-21 21:55:02 +0530

    Ansible

---
+++ vars.tf
@@ -0,0 +1,11 @@
+variable "env" {}
+variable "component" {}
+variable "instance_type" {}
+variable "vpc_id" {}
+variable "app_port" {}
+variable "vpc_cidr" {}
+variable "subnets" {}
+variable "bastion_node_cidr" {}
+variable "instance_count" {}
+variable "prometheus_cidr" {}
+variable "kms_key_id" {}

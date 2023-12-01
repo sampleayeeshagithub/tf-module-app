2aa30b6e238b91dd8f046aae88b80ba2af925c4b
Mulla Ayeesha <ayeeshamulla0708@gmailcom>
2023-12-01 19:12:17 +0530

    Ansible

--- main.tf
+++ main.tf
@@ -185,7 +185,7 @@ resource "aws_lb_listener_rule" "rule" {
 
   condition {
     host_header {
-      values = ["${var.component}-${var.env}.rdevopsb73.online"]
+      values = ["${var.component}-${var.env}.ayeeshadevops75.online"]
     }
   }
 }
